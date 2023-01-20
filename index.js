4d8246ac695615d522ca38b24e1b50c0fac9e1d2
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-20 16:46:10 -0300

    Apply calcConvertion variaable for better readability

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-//converter moeda
-let usdValue = document.getElementById("value-usd") 
-let brlValue = document.getElementById("value-brl")
+const usdValue = document.getElementById("value-usd") 
+const brlValue = document.getElementById("value-brl")
 
 usdValue.addEventListener("input", function exchangeInBRL(event){
-    const brValueasNumber = Number(event.target.value)
-    brlValue.value= (brValueasNumber * 5.21).toFixed(2)
+    const brValueAsNumber = Number(event.target.value)
+    const calcConvertion = (brValueAsNumber * 5.21).toFixed(2)
+    brlValue.value = calcConvertion
 })
 
 brlValue.addEventListener("input", function exchangeInUSD(event){
-    const usdValueasNumber = Number(event.target.value)
-    usdValue.value = (usdValueasNumber / 5.21).toFixed(2)
+    const usdValueAsNumber = Number(event.target.value)
+    const calcConvertion = (usdValueAsNumber / 5.21).toFixed(2)
+    usdValue.value = calcConvertion
 })
-
-
-  
