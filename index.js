0ab047fc225a71330619e95aa5c31d31de2f750d
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-20 11:48:11 -0300

    Adjust the input responsivity in JS hide the number input with css

--- index.js
+++ index.js
@@ -3,25 +3,14 @@ let usdValue = document.getElementById("value-usd")
 let brlValue = document.getElementById("value-brl")
 
 usdValue.addEventListener("input", function exchangeInBRL(event){
-
-    const brValueAsNumber = Number(event.target.value)
-     const calcConvertion = (brValueAsNumber * 5.15).toFixed(2)
-     const currencyUsdFormat = new Intl.NumberFormat('pt-BR', { 
-        style: 'currency', 
-        currency: 'BRL',
-        
-    });
-
-    brlValue.value = currencyUsdFormat.format(calcConvertion)
+    const brValueasNumber = Number(event.target.value)
+    brlValue.value= (brValueasNumber * 5.21).toFixed(2)
 })
 
 brlValue.addEventListener("input", function exchangeInUSD(event){
-    const usdValueAsNumber = Number(event.target.value)
-    const calcConvertion = (usdValueAsNumber / 5.15).toFixed(2)
-    const currencyBrlFormat = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',     
-    });
-
-    usdValue.value = currencyBrlFormat.format(calcConvertion)
+    const usdValueasNumber = Number(event.target.value)
+    usdValue.value = (usdValueasNumber / 5.21).toFixed(2)
 })
+
+
+  
