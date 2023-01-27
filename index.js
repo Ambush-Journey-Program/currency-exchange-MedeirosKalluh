d6b5083ce60949592cc7802b2fa1f2c30d74d3f9
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-27 11:19:04 -0300

    Implement the dollar API to apply dinamic dollar value

--- index.js
+++ index.js
@@ -2,21 +2,28 @@
 const usdValue = document.getElementById("value-usd") 
 const brlValue = document.getElementById("value-brl")
 
-usdValue.addEventListener("input", function exchangeInBRL(event){
+async function fetchResponse(url) {
+    const response = await fetch(url);
+    const body = await response.json();
+    return body.USDBRL.ask
+} 
+
+usdValue.addEventListener("input", async function exchangeInBRL(event){
+    const usdExchange = await fetchResponse(`https://economia.awesomeapi.com.br/last/USD-BRL`)
     const brValueAsNumber = Number(event.target.value)
-    const calcConvertion = (brValueAsNumber * 5.08).toFixed(2)
+    const calcConvertion = (brValueAsNumber * usdExchange).toFixed(2)
     brlValue.value = calcConvertion
 })
 
-brlValue.addEventListener("input", function exchangeInUSD(event){
+brlValue.addEventListener("input", async function exchangeInUSD(event){
+    const usdExchange = await fetchResponse(`https://economia.awesomeapi.com.br/last/USD-BRL`)
     const usdValueAsNumber = Number(event.target.value)
-    const calcConvertion = (usdValueAsNumber / 5.08).toFixed(2)
+    const calcConvertion = (usdValueAsNumber / usdExchange).toFixed(2)
     usdValue.value = calcConvertion
 })
 
-const currencyWrapperEl = document.querySelectorAll(".currency")
-
 //Switch Button
+const currencyWrapperEl = document.querySelectorAll(".currency")
 const currencyContainer = document.querySelector("#container")
 const button = document.getElementById("switch-button")
 button.addEventListener("click", function switchCurrency(event){
@@ -24,7 +31,6 @@ button.addEventListener("click", function switchCurrency(event){
     currencyWrapperEl.forEach(function(element){
         element.classList.toggle("currency--reverse")
     })
-        
     const inputs = document.querySelectorAll(".currency__input")
     const usdInput = inputs[0]
     const brlInput = inputs [1]
